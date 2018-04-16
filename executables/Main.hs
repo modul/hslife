038a06e7be784e2d8813bb36bc61750d7e8a38f2
Giermann <giermanr@de.bertrandt.net>
2018-04-16 17:04:04 +0200

    full terminal control for a better display

--- executables/Main.hs
+++ executables/Main.hs
@@ -5,29 +5,35 @@ import System.Random
 import System.Environment
 import System.Exit
 import System.Info (os)
-import System.Process (callCommand)
 import Control.Concurrent (threadDelay)
+import Control.Exception
 
-fps = 60
+import System.Console.ANSI
+
+fps = 30
 width = 80
 height = 40
 
-draw str = callCommand (clear os) >> putStrLn str
-    where clear "linux" = "clear"
-          clear _       = "cls"
+main :: IO ()
+main = do
+    setTitle "Game of Life"
+    clearScreen
+    game fps height width showWorld `catch` \UserInterrupt -> exitGame
 
+game fps h w display = do
+    rng <- getStdGen
+    let world = mkRandWorld rng h w
+    mapM_ (frame fps . display) (generate world)
+    where generate = iterate generation
+
+exitGame = do
+    clearScreen
+    setCursorPosition 0 0
+
+draw str = do
+    hideCursor
+    setCursorPosition 0 0
+    putStrLn str
 
 frame speed content = let delay = round (1e6 / speed)
                        in draw content >> threadDelay delay
-
-generate = iterate generation
-
-showw = showWorldUsing defaultLifeSign
-
-main :: IO ()
-main = do
-    --getArgs >>= (putStrLn . show)
-    rng <- getStdGen
-    let world = mkRandWorld rng height width
-    mapM_ (frame fps . showw) (generate world)
-
