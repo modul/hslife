66ca1ab361e202a1eefc232e1b11ae0e9b00521c
Remo Giermann <remo.giermann@de.bertrandt.com>
2018-04-16 13:42:52 +0200

    sane printing of single frames, with speed control!

--- executables/Main.hs
+++ executables/Main.hs
@@ -4,17 +4,30 @@ import Life
 import System.Random
 import System.Environment
 import System.Exit
+import System.Info (os)
+import System.Process (callCommand)
+import Control.Concurrent (threadDelay)
 
+fps = 60
 width = 80
 height = 40
-showw = showWorldUsing defaultLifeSign
+
+draw str = callCommand (clear os) >> putStrLn str
+    where clear "linux" = "clear"
+          clear _       = "cls"
+
+
+frame speed content = let delay = round (1e6 / speed)
+                       in draw content >> threadDelay delay
 
 generate = iterate generation
+
+showw = showWorldUsing defaultLifeSign
 
 main :: IO ()
 main = do
     --getArgs >>= (putStrLn . show)
     rng <- getStdGen
     let world = mkRandWorld rng height width
-    mapM_ (putStrLn . showw) (generate world)
-  
+    mapM_ (frame fps . showw) (generate world)
+
